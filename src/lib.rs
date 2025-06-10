1f1f70588cdf9aa1cc553cf64bcf16ee9914955c
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-10 12:30:35 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
 #![allow(dead_code)]
+#![allow(non_upper_case_globals)]
 
 mod config {
     const PORT: u16 = 12345;
 }
 
+const numbers: [u8; 6] = [0u8, 1, 2, 3, 4, 5];
+
 pub fn hello() {
     println!("Hello, world!");
+    for n in numbers {
+        println!("{}", n);
+    }
+    println!("{:?}", numbers);
 }
