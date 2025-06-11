94dd070740aa56b0f4932413f8b4eef414436de2
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-11 12:08:15 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
 // https://masteringbackend.com/posts/actix-web-the-ultimate-guide#complete-actix-overview
 
 mod config;
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, Responder, delete, get, post, put};
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    "Hello, Actix web!"
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to the index page!")
+}
+
+#[post("/create")]
+async fn create() -> impl Responder {
+    HttpResponse::Created().body("Resource created successfully!")
+}
+
+#[put("/update")]
+async fn update() -> impl Responder {
+    HttpResponse::Ok().body("Resource updated successfully!")
+}
+
+#[delete("/delete")]
+async fn delete() -> impl Responder {
+    HttpResponse::NoContent().finish()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server started at http://{}:{}", config::HOST, config::PORT);
     HttpServer::new(|| {
-        App::new().service(hello)
+        App::new()
+            .service(index)
+            .service(create)
+            .service(update)
+            .service(delete)
     })
-        .bind(config::BIND)?
-        .run()
-        .await
+    .bind(config::BIND)?
+    .run()
+    .await
 }
